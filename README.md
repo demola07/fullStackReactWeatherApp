11206d0ccb26624d9eefe347f7b09ce20d1ea31a
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-23 03:00:34 +0100

    Code clean up to effect best practices and readme update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fullStackReactWeatherApp
+# Weather App
+
+A simple weather app, which displays the current weather forcast of your selected location
+Built using React, Express and PostgreSQL.
+
+##Checkout the [Weather App](https://weather-app-aas-2019.herokuapp.com/)
--- server/models/cities.js
+++ server/models/cities.js
@@ -2,19 +2,20 @@ const db = require("../database");
 
 class Cities {
   static retrieveALl(callback) {
-    db.query("select city_name from cities", function(err, res) {
+    db.query("select city_name from cities", (err, res) => {
       if (err.error) return callback(err);
       callback(res);
     });
   }
   static insert(city, callback) {
-    db.query("INSERT INTO cities (city_name) VALUES ($1)", [city], function(
-      err,
-      res
-    ) {
-      if (err.error) return callback(err);
-      return callback(res);
-    });
+    db.query(
+      "INSERT INTO cities (city_name) VALUES ($1)",
+      [city],
+      (err, res) => {
+        if (err.error) return callback(err);
+        return callback(res);
+      }
+    );
   }
 }
 
